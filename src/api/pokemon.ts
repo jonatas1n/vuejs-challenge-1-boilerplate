120a5e2c13b280deaba4fe029d3b8f67cf6a820b
jonatas1n <jonatasgeek@gmail.com>
2024-11-04 17:41:30 -0300

    feat: add api

---
+++ src/api/pokemon.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+type stringOrNumber = string | number;
+
+const apiClient = axios.create({
+  baseURL: 'https://pokeapi.co/api/v2/',
+  timeout: 10000,
+});
+
+export default {
+  async fetchPokemons(offset: number = 0, limit: number = 20) {
+    const response = await apiClient.get(`pokemon?limit=${limit}&offset=${offset}`);
+    const { results, count, next, previous } = response.data;
+    
+    const offsetNext = new URL(next).searchParams.get('offset');
+    const offsetPrev = previous ? new URL(previous).searchParams.get('offset') : null;
+
+    return { results, count, offset, offsetNext, offsetPrev };
+  },
+
+  fetchPokemonDetails(id: stringOrNumber) {
+    return apiClient.get(`pokemon/${id}`);
+  },
+
+  fetchPokemonSpecies(id: number) {
+    return apiClient.get(`pokemon-species/${id}`);
+  }
+}
