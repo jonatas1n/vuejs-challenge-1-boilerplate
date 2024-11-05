1e651c32f76ad03bb2190a0a8cf8085de2d49ebd
jonatas1n <jonatasgeek@gmail.com>
2024-11-04 23:10:31 -0300

    feat: add link button to details page

--- src/components/PokemonDisplay.vue
+++ src/components/PokemonDisplay.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <h1>{{ pokemon?.id }} - {{ pokemon?.name }}</h1>
-    <h2>Height: {{ pokemon?.height }}</h2>
-    <h2>Weight: {{ pokemon?.weight }}</h2>
-    <p>{{ pokemon?.flavorText }}</p> 
+    <h1>{{ pokemon.id }} - {{ pokemon.name }}</h1>
+    <h2>Height: {{ pokemon.height }}</h2>
+    <h2>Weight: {{ pokemon.weight }}</h2>
+    <p>{{ pokemon.flavorText }}</p> 
   </div>
   <div>
-    
+    <RouterLink :to="{name: 'pokemon-details', params: { id: pokemon.id }}">View</RouterLink>
   </div>
 </template>
 
 <script setup>
+  import { RouterLink } from 'vue-router';
+
   defineProps({
     pokemon: {
       type: Object,
